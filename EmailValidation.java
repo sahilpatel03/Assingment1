aa301b20328f053a6aeb1df2236d60b6ee035956
sahilpatel03 <34380147+sahilpatel03@users.noreply.github.com>
2022-01-23 23:49:11 +0530

    Add files via upload

---
+++ EmailValidation.java
@@ -0,0 +1,29 @@
+package SubmitinEl;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class EmailValidation {
+	public static boolean isValidEmail(String email) {
+		String regex = "^(.+)@(.+)$";
+		// initialize the Pattern object
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(email);
+		return matcher.matches();
+
+	}
+
+	public static void main(String[] args) {
+
+		System.out.println("Enter any email address to check");
+		Scanner sc = new Scanner(System.in);
+		String input = sc.nextLine();
+		System.out.println("The Email address " + input + " is " + (isValidEmail(input) ? "valid" : "invalid"));
+
+	}
+
+}
